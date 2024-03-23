fc84010c64716984ff9e9a93b65efb88e2fee028
Sviridov Vlad <vsviridov@vsegda-da.com>
2024-03-23 11:03:52 +0300

    end commit

--- src/main/java/com/onedayoffer/taskdistribution/web/api/v1/controller/ControllerExceptionHandler.java
+++ src/main/java/com/onedayoffer/taskdistribution/web/api/v1/controller/ControllerExceptionHandler.java
@@ -5,7 +5,7 @@ package com.onedayoffer.taskdistribution.web.api.v1.controller;
  * @version 1.0.0-SNAPSHOT
  * @since 2024-03-23
  */
-// ToDo: soon
+// ToDo: soon (too late)
 //@ControllerAdvice(annotations= RestController.class)
 public class ControllerExceptionHandler {
 
